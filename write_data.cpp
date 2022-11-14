3155e4fd4a331c552dcbf67323618341c1b38ae3
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2022-11-14 18:25:23 +0100

    data generation

--- write_data.cpp
+++ write_data.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
+#include <random>
+
+#include "boost/container/map.hpp"
+//#include "metall/metall.hpp"
+
+using map_t = boost::container::map<int, int>;
+
+void FillMap(map_t& m) {
+    std::default_random_engine r{};
+    std::uniform_int_distribution d{ 0,0x5000 };
+
+    for (int i = 0; i < 0x100; ++i)
+        m[d(r)] = d(r);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    map_t m{};
+    FillMap(m);
+
+
+    std::cout << "write writing data:" << std::endl;
+    for (const auto& kv : m)
+        std::cout << kv.first << "=" << kv.second << std::endl;
+    std::cout << "number of k/v pairs: " << m.size() << std::endl;
+
     return 0;
 }
