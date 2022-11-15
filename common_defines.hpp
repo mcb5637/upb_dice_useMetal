8ebaca0765bcd32dfc06c198b6ada86f48f8bc2b
mcb <mgrbo@web.de>
2022-11-15 20:51:54 +0100

    some template magic

--- common_defines.hpp
+++ common_defines.hpp
@@ -1,9 +1,13 @@
 
+#include "boost/container/options.hpp"
 #include "boost/container/map.hpp"
 #include "metall/metall.hpp"
 
 namespace useMetall {
-	using map_t = boost::container::map<int, int, std::less<int>, metall::manager::allocator_type<std::pair<const int, int>>>;
+	template<class K, class V, class Compare = std::less<K>, class Options = boost::container::tree_assoc_defaults>
+	using MapWithMetallAlloc = boost::container::map<K, V, Compare, metall::manager::allocator_type<std::pair<const K, V>>, Options>;
+
+	using map_t = MapWithMetallAlloc<int, int>;
 
 	constexpr const char* Path = "/tmp/datastore";
 	constexpr const char* ObjectName = "map";
